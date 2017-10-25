62a45b0ea50abfdca30d38812f3710ae8811dbfd
Nicholas Arnaud <cobrahead09@gmail.com>
2017-10-25 01:08:38 -0500

    Learning landmass precedural generation

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode
+    {
+        NoiseMap, ColorMap
+    }
+    public DrawMode drawMode;
+
+    public int width;
+    public int height;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(width, height, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        Color[] colorMap = new Color[width * height];
+        for(int y = 0; y < height; y++)
+        {
+            for (int x = 0;  x < width; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++ )
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * width + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColorMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, width, height));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (width < 1)
+            width = 1;
+        if (height < 1)
+            height = 1;
+        if (lacunarity < 1)
+            lacunarity = 1;
+        if (octaves < 0)
+            octaves = 0;
+    }
+}
+
+[Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+
+    public static float[,] GenerateNoiseMap(int width, int height, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
+    {
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        float[,] noiseMap = new float[width, height];
+
+
+        for (int i = 0; i < octaves; i ++)
+        {
+            float offsetX = prng.Next(-10000, 10000) + offset.x;
+            float offsetY = prng.Next(-10000, 10000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+        
+        if (scale <= 0)
+        {
+            scale = 0.001f;
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfwidth = width / 2;
+        float halfheight = height / 2;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = (x - halfwidth) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y - halfheight) / scale * frequency + octaveOffsets[i].y;
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseMap[x, y] = perlinValue;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+        return noiseMap;
+    }
+
+}
